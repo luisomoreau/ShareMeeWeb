409f54530d8ec94e227bbce7d9137910cad43585
Marin <marinbernaert@aol.com>
2015-04-26 14:42:57 +0200

    Add Object Fonction change mysql request

--- webservice/model/add_object.php
+++ webservice/model/add_object.php
@@ -44,7 +44,7 @@ if (isset($_POST['nameObject']) && isset($_POST['descObject']) /*&& isset($_POST
 
     // mysql update row with matched pid
     $result = mysql_query("INSERT INTO smobject (idObject, nameObject, brandObject, descObject, latObject, longObject, yearObject, imagePath1Object, imagePath2Object, imagePath3Object, addedDateTimeObject, smCity_idCity, smUser_idUser, smCategory_idCategory) VALUES (NULL,'$nameObject',NULL,'$descObject', $latObject, $longObject,NULL ,NULL ,NULL ,NULL ,NULL,NULL ,$smUser_idUser,$smCategory_idCategory)");
-
+    $erreur = mysql_error($db);
     // check if row inserted or not
     if ($result) {
         // successfully updated
@@ -56,6 +56,7 @@ if (isset($_POST['nameObject']) && isset($_POST['descObject']) /*&& isset($_POST
     } else {
         $response["success"] = 2;
         $response["message"] = "Requête incorrecte.";
+        $response["erreur"]= $erreur;
         $response["fields"]=array();
         array_push($response["fields"], $fields);
         $response["champs"]=array();
