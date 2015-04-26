92b2b29b771cd4e99aee795803abfbe6ddfb1e32
Marin <marinbernaert@aol.com>
2015-04-26 12:47:47 +0200

    Add Object Fonction with good files

--- webservice/model/add_object.php
+++ webservice/model/add_object.php
@@ -1,4 +1,3 @@
-
 <?php
 
 // array for JSON response
@@ -7,19 +6,26 @@ $response = array();
 $fields = array();
 $fields['nameObject']=$_POST['nameObject'];
 $fields['descObject']=$_POST['descObject'];
+$fields['latObject'] = $_POST['latObject'];
+$fields['longObject'] = $_POST['longObject'];
 //$fields['imagePathObject']=$_POST['imagePathObject'];
-//$fields['smCategory_idCategory']=$_POST['smCategory_idCategory'];
-//$fields['smUser_idUser']=$_POST['smUser_idUser'];
+$fields['smUser_idUser']=$_POST['smUser_idUser'];
+$fields['smCategory_idCategory']=$_POST['smCategory_idCategory'];
+
 
 
 // check for required fields
-if (isset($_POST['nameObject']) && isset($_POST['descObject']) /*&& isset($_POST['imagePathObject']) && isset($_POST['smCategory_idCategory'])&& isset($_POST['smUser_idUser'])*/) {
+if (isset($_POST['nameObject']) && isset($_POST['descObject']) /*&& isset($_POST['imagePathObject'])*/ && isset($_POST['smCategory_idCategory'])&& isset($_POST['smUser_idUser'])) {
 
     $nameObject = $_POST['nameObject'];
     $descObject = $_POST['descObject'];
+    $latObject = $_POST['latObject']+0.0;
+    $longObject = $_POST['longObject']+0.0;
     //$imagePathObject = $_POST['imagePathObject'];
-    //$smCategory_idCategory = $_POST['smCategory_idCategory'];
-    //$smUser_idUser = $_POST['smUser_idUser'];
+    $smUser_idUser = $_POST['smUser_idUser'];
+    $smCategory_idCategory = $_POST['smCategory_idCategory'];
+
+
     $dateRegistration;
 
 // include db connect class
@@ -30,7 +36,7 @@ if (isset($_POST['nameObject']) && isset($_POST['descObject']) /*&& isset($_POST
     mysql_query('SET CHARACTER SET utf8');
 
     // mysql update row with matched pid
-    $result = mysql_query("INSERT INTO smobject (`idObject`, `nameObject`, `brandObject`, `descObject`, `latObject`, `longObject`, `yearObject`, `imagePath1Object`, `imagePath2Object`, `imagePath3Object`, `addedDateTimeObject`, `smCity_idCity`, `smUser_idUser`, `smCategory_idCategory`) VALUES (NULL, '$nameObject', NULL, '$descObject', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,NULL,NULL)");
+    $result = mysql_query("INSERT INTO smobject (`idObject`, `nameObject`, `brandObject`, `descObject`, `latObject`, `longObject`, `yearObject`, `imagePath1Object`, `imagePath2Object`, `imagePath3Object`, `addedDateTimeObject`, `smCity_idCity`, `smUser_idUser`, `smCategory_idCategory`) VALUES (NULL, '$nameObject', NULL, '$descObject', $latObject, $longObject, NULL, NULL, NULL, NULL, NOW(), NULL,'$smUser_idUser','$smCategory_idCategory')");
 
     // check if row inserted or not
     if ($result) {
@@ -43,6 +49,8 @@ if (isset($_POST['nameObject']) && isset($_POST['descObject']) /*&& isset($_POST
     } else {
         $response["success"] = 2;
         $response["message"] = "Requête incorrecte.";
+
+        echo json_encode($response);
     }
 } else {
     // required field is missing
@@ -54,4 +62,4 @@ if (isset($_POST['nameObject']) && isset($_POST['descObject']) /*&& isset($_POST
     // echoing JSON response
     echo json_encode($response);
 }
-?>
+
