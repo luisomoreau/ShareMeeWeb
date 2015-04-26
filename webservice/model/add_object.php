902c8acd8b7bb7e7b6dab63fb73bba73ddca81ae
Marin <marinbernaert@aol.com>
2015-04-26 13:13:12 +0200

    Add Object Fonction with good files and change for function NOW

--- webservice/model/add_object.php
+++ webservice/model/add_object.php
@@ -36,20 +36,18 @@ if (isset($_POST['nameObject']) && isset($_POST['descObject']) /*&& isset($_POST
     mysql_query('SET CHARACTER SET utf8');
 
     // mysql update row with matched pid
-    $result = mysql_query("INSERT INTO smobject (`idObject`, `nameObject`, `brandObject`, `descObject`, `latObject`, `longObject`, `yearObject`, `imagePath1Object`, `imagePath2Object`, `imagePath3Object`, `addedDateTimeObject`, `smCity_idCity`, `smUser_idUser`, `smCategory_idCategory`) VALUES (NULL, '$nameObject', NULL, '$descObject', $latObject, $longObject, NULL, NULL, NULL, NULL, NULL, NULL,'$smUser_idUser','$smCategory_idCategory')");
+    $result = mysql_query("INSERT INTO smobject (`nameObject`, `descObject`, `latObject`, `longObject`,  `smUser_idUser`, `smCategory_idCategory`) VALUES ('$nameObject','$descObject', $latObject, $longObject,'$smUser_idUser','$smCategory_idCategory')");
 
     // check if row inserted or not
     if ($result) {
         // successfully updated
         $response["success"] = 1;
-        $response["test Now"] = NOW();
         $response["message"] = "Objet ajoute.";
 
         // echoing JSON response
         echo json_encode($response);
     } else {
         $response["success"] = 2;
-        $response["test Now"] = NOW();
         $response["message"] = "Requête incorrecte.";
 
         echo json_encode($response);
