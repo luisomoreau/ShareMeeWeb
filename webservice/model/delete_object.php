a228d6d26958f10f90d9223f23379a1bfc25faf1
Marin <marinbernaert@aol.com>
2015-04-27 23:40:26 +0200

    Delete Object Function

---
+++ webservice/model/delete_object.php
@@ -0,0 +1,48 @@
+<?php
+
+// array for JSON response
+$response = array();
+
+$fields = array();
+$fields['idObject']=$_POST['idObject'];
+
+
+// check for required fields
+if (isset($_POST['idObject'])) {
+
+    $idObject = $_POST['idObject'];
+
+
+// include db connect class
+    require '../controller/db_connect.php';
+
+// connecting to db
+    $db = new DB_CONNECT();
+    mysql_query('SET CHARACTER SET utf8');
+
+    // mysql update row with matched pid
+    $result = mysql_query("DELETE FROM smObject WHERE idObject=$idObject");
+
+    // check if row inserted or not
+    if ($result) {
+        // successfully updated
+        $response["success"] = 1;
+        $response["message"] = "Object Supprimé.";
+
+        // echoing JSON response
+        echo json_encode($response);
+    } else {
+        echo mysql_error();
+        $response["success"] = 2;
+        $response["message"] = "Requête incorrecte.";
+    }
+} else {
+    // required field is missing
+    $response["success"] = 0;
+    $response["message"] = "Required field(s) is missing";
+    $response["fields"]=array();
+    array_push($response["fields"], $fields);
+
+    // echoing JSON response
+    echo json_encode($response);
+}
